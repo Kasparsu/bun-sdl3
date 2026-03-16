d03c3b9269639815aca63d30414c24ca216311ab
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-03-16 23:05:58 +0200

    renderer: update renderDebugText signature (renderer, x, y, textPtr)

--- src/SDL/renderer.ts
+++ src/SDL/renderer.ts
@@ -179,8 +179,8 @@ export function present(renderer: any): boolean {
   return Boolean(sdl.SDL_RenderPresent(renderer as any));
 }
 
-export function renderDebugText(renderer: any, textPtr: Pointer, x: number, y: number, color: number, fontSize: number): boolean {
-  return Boolean((sdl as any).SDL_RenderDebugText(renderer as any, textPtr as any, x, y, color, fontSize));
+export function renderDebugText(renderer: any, x: number, y: number, textPtr: Pointer): boolean {
+  return Boolean((sdl as any).SDL_RenderDebugText(renderer as any, x, y, textPtr as any));
 }
 
 export function addVulkanRenderSemaphores(renderer: any, waitSemaphoresPtr: Pointer, signalSemaphoresPtr: Pointer): boolean {
